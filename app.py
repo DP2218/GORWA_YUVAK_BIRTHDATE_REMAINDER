78d657d7373fe2fcf48974325a1b4570798e6d41
DP2218 <pdivy292@gmail.com>
2026-04-18 14:54:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ if selected_month != "Select Month":
 
     st.markdown("### 🎂 Birthday List")
 
-month_df = df[df['Month'] == selected_month].copy()
-month_df = month_df.sort_values(by='Birthdate')
+month_df['Day'] = month_df['Birthdate'].dt.day
+month_df = month_df.sort_values(by='Day')
     month_df['Birthdate'] = month_df['Birthdate'].dt.strftime('%d %B')
 
     if not month_df.empty:
